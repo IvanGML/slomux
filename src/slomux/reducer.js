45922ff3773c74074ae92611f344693d7991c7e8
IvanGML <somovlife@gmail.com>
2019-05-08 15:05:56 +0300

    improve reducer and add actions

--- src/slomux/reducer.js
+++ src/slomux/reducer.js
@@ -1,9 +1,33 @@
-import { CHANGE_INTERVAL } from './constants';
+import { CHANGE_INTERVAL, CHANGE_TIMER_STATE, CHANGE_TIMER_TIME, RESET_INTERVAL } from './constants';
 
-export default (state, action) => {
+const initialState = {
+    interval: 1000,
+    timerTime: 0,
+    isStarted: false,
+}
+
+export default (state = initialState, action) => {
+    const { interval, isStarted, timerTime} = action;
     switch (action.type) {
         case CHANGE_INTERVAL:
-            return state += action.payload;
+            return {
+                ...state,
+                interval: state.interval + interval
+            };
+        case CHANGE_TIMER_STATE:
+            return {
+                ...state,
+                isStarted
+            };
+        case CHANGE_TIMER_TIME:
+            return {
+                ...state,
+                timerTime: state.timerTime + timerTime
+            };
+        case RESET_INTERVAL:
+            return {
+                ...initialState
+            };
         default:
             return state;
     };
