997b971e2042c99fcf011f417bd8c7a6e397e9af
IvanGML <somovlife@gmail.com>
2019-05-08 15:04:20 +0300

    screateStore improvement

--- src/slomux-library/createStore.js
+++ src/slomux-library/createStore.js
@@ -2,32 +2,36 @@
  * validate an action
  */
 const validateAction = action => {
-  if (!action || typeof action !== 'object' || Array.isArray(action)) {
-      throw new Error('Called action isn\'t an object');
-  }
-  if (typeof action.type === 'undefined') {
-      throw new Error('Action must have a type');
-  }
+    if (!action || typeof action !== 'object' || Array.isArray(action)) {
+        throw new Error('Called action isn\'t an object');
+    }
+    if (typeof action.type === 'undefined') {
+        throw new Error('Action must have a type');
+    }
 };
 
-const createStore = (reducer) => {
-    let currentState = 1000;
-    const listeners = [];
-
-    const getState = () => currentState;
+const createStore = reducer => {
+    let currentState;
+    const subscribers = [];
     const dispatch = action => {
         validateAction(action);
-        currentState = reducer(currentState, action)
-        listeners.forEach(listener => listener())
-    };
-
-    const subscribe = listener => listeners.push(listener);
-
+        currentState = reducer(currentState, action);
+        subscribers.forEach(callback => callback());
+    }
+    const getState = () => currentState;
+    const subscribe = callback => {
+        subscribers.push(callback);
+        return () => subscribers.filter(cb => cb !== callback);
+    }
+    dispatch({
+        type: 'initialize'
+    });
     return {
         getState,
         dispatch,
         subscribe
     };
-}
+};
+
 
 export default createStore;
